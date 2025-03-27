4b87d0153ff250be40bc85c69c0d4480c0d902c0
IvanNeDivan <155850328+IvanNeDivan@users.noreply.github.com>
2025-03-28 00:58:31 +0500

    Update parser.py
    - Добавил/подправил #комментарии по коду
    - Поправил костыль с http(s). Теперь функция работает с любым адресом.
    - Изменил некоторый текст ошибок
    - Изменил результат работы функции (смотри комментарии над if __name__ )
    - Добавил примеры работы программы в if __name__

--- parser.py
+++ parser.py
@@ -7,12 +7,13 @@ import asyncio
 import os
 
 
-async def _fetch(s: aiohttp.ClientSession, url: str):
+async def _fetch(s: aiohttp.ClientSession, url: str) -> str:
     async with s.get(url) as r:
         return await r.read()
 
 
-async def _fetch_all(s: aiohttp.ClientSession, img_urls: list):
+# Подумать о возвращения статуса парсинга изображения
+async def _fetch_all(s: aiohttp.ClientSession, img_urls: list) -> list:
     tasks = []
     for url in img_urls:
         task = asyncio.create_task(_fetch(s, url))
@@ -22,7 +23,7 @@ async def _fetch_all(s: aiohttp.ClientSession, img_urls: list):
 
 
 def _fetch_img_links(url: str) -> list:
-    page = requests.get(url)
+    page = requests.get(url)  # Тут может все сломаться
     soup = BeautifulSoup(page.text, 'lxml')
     # Формирование списка со всеми ссылками на изображения
     image_links = []
@@ -31,7 +32,7 @@ def _fetch_img_links(url: str) -> list:
     domain = re.search(r'https?://[^/]+', url).group()
     http = re.search(r'https?:', url).group()
     for item in soup.find_all('img'):
-        # Поиск ссылки в img
+        # Поиск ссылки на изображение в img
         link = item.get('data-src') or item.get('src')
         if not link:
             # Ссылка не найдена
@@ -63,37 +64,34 @@ def _check_website_access(url: str) -> str:
         return f'Failed to access the site: {e}'
 
 
-async def _parser(url: str) -> str:
-    # Сохраняю ошибки, если не удалось скачать файл (пока не используется)
+async def _parser(url: str) -> tuple:
+    # Сохраняю ошибки, если не удалось запарсить/скачать фото
     download_error = []
 
     # Очистка ссылки от лишних пробелов и добавляем http://
     url = url.strip()
-    if 'http' not in url:
+    if not url.startswith('http'):
         url = 'http://' + url
-    if url[-1] != '/':  # Жосткий костыль (исправлю). Без этого проблемы с сайтами без http
-        url += '/'
 
     # Проверка доступа к сайту
     status = _check_website_access(url)
     if status != 'done':
-        return status
+        return 'error', status
 
     # Массив из ссылок на изображения
     image_links = _fetch_img_links(url)
     if len(image_links) == 0:
-        return "No images found on the site! Suitable format png, jpeg, jpg"
+        return 'error', 'No images found on the site! Suitable format png, jpeg, jpg'
 
-    # Асинхронное получение двоичных изображений
+    # Асинхронное получение изображений в двоичном представление
     try:
         async with aiohttp.ClientSession(trust_env=True) as session:
             imgs = await _fetch_all(session, image_links)
     except Exception as e:
-        return f"Failed to retrieve the image. Try again. {e}"
+        return 'error', f'Failed to retrieve the image. Try again. {e}'
 
     # Путь images/parsed_images/название_изображения
     path_parsed_images = 'images/parsed_images'
-    path = os.path.join(path_parsed_images, url.split("/")[-1])
 
     # Очищаем папку parsed_images
     if os.path.exists(path_parsed_images):
@@ -104,39 +102,53 @@ async def _parser(url: str) -> str:
 
         # Создаем папку с именем файла
         filename = img_url.split('/')[-1]  # Имя файла с расширением
-        file_folder = os.path.join(path, filename)
+        file_folder = os.path.join(path_parsed_images, filename)
         if os.path.exists(file_folder):  # Если попался файл с существующим названием
             c = 2
             while os.path.exists(file_folder + f' ({c})'):
                 c += 1
             file_folder += f' ({c})'
+        os.makedirs(file_folder, exist_ok=True)  # Создание папки с именем файла
 
-        os.makedirs(file_folder, exist_ok=True)  # Создаем папку с именем файла
+        # Если не удалось запарсить изображение
+        if img is None:
+            download_error.append(f"Failed to download a file: {img_url}")
+            shutil.rmtree(file_folder)  # Удаляем созданную папку для этого файла
+            continue
 
         try:  # Загружаем изображения
             file_extension = filename.split('.')[-1]
-            if img is None:
-                raise IOError("Failed to download file")
             with open(os.path.join(file_folder, f'default.{file_extension}'), 'wb') as f:
                 f.write(img)
-
-            # print(f'Скачано: {img_url}')
         except Exception as e:
             shutil.rmtree(file_folder)  # Удаляем созданную папку для этого файла
-            download_error.append(f'Failed to download file {e}')
+            download_error.append(f'Error when writing a file: {img_url} {e}')
 
-    return 'done'
+    return ('done', 'OK') if not download_error else ('warning', '\n'.join(download_error))
 
 
-def scrape_and_save_images(url: str) -> str:
+def scrape_and_save_images(url: str) -> tuple:
     return asyncio.run(_parser(url))
 
 
-# Руслан пользуется только функцией scrape_and_save_images(), которая на вход ожидает url
-# Результат функции 'done', если все успешно. В остальных случаях результат - передастся текст с ошибкой
-# Путь сохранение изображений как в тг в пункте "Хранение изображений"
+# ТГ бот пользуется только функцией scrape_and_save_images(), которая на вход ожидает url
 
-# Пример работы с выводом результата функции на экран
+# Результат функции кортеж (status, message)
+# status = done/error/warning
+# message = OK/текст ошибки/ текст предупреждения
+# Пометка: done/warning - значит, что удалось запарсить и скачать изображения.
+#                         В message warning - из-за чего не получилось скачать/запарсить изображение и ссылка на него
+
+# Примеры работы с выводом результата функции на экран
 if __name__ == '__main__':
     address = 'https://scrapingclub.com/exercise/list_basic/'
     print(scrape_and_save_images(address))
+    # output: ('done', 'OK')
+
+    # address = 'https://texterra.ru/blog/kak-sdelat-iz-stranitsy-404-chto-to-poleznoe-i-interesnoe-priаmery.html'
+    # print(scrape_and_save_images(address))
+    # # output: ('error', 'Failed to access the site: status_code 404')
+
+    # Не нашел пример сайта результатом функции ('waring', message). Но вот, пример, что ожидается
+    # output: ('warning', 'Failed to download a file: https://scrapingclub.com/exercise/list_basic_detail/96436-A/\n
+    # Failed to download a file: https://scrapingclub.com/exercise/list_basic_detail/90008-E/')
